f4a338af48ccfec499bb1405355539397525c991
Antoine <gourgue.antoine@esh64.fr>
2023-03-18 20:01:35 +0100

    added invoice with id in URL

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -7,40 +7,35 @@ use App\Entity\CookingTime;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/invoice/{user_cooking_time_id}", name="invoice")
+ */
 class InvoiceController extends AbstractController
 {
-    public function invoice(Request $request): Response
+    public function invoice(Request $request, int $user_cooking_time_id): Response
     {
-// Récupérer l'utilisateur connecté
+        // Récupérer l'utilisateur connecté
         $user = $this->getUser();
 
-// Récupérer les informations de l'achat
-        $achatId = $request->query->get('user_cooking_time_id');
-        $achat = $this->getDoctrine()->getRepository(UserCookingTime::class)->find($achatId);
+        // Récupérer les informations de l'achat
+        $achat = $this->getDoctrine()->getRepository(UserCookingTime::class)->find($user_cooking_time_id);
 
-// Récupérer les informations du produit associé à l'achat
-        $produit = $this->getDoctrine()->getRepository(CookingTime::class)->find($achat->getCookingTimeId());
+        // Vérifier si l'achat appartient à l'utilisateur connecté
+        if (!$achat || $achat->getUser() !== $user) {
+            throw $this->createNotFoundException('Achat non trouvé');
+        }
 
-// Générer la facture
-        $facture = 'Facture pour l\'achat effectué le ' . $achat->getDate()->format('d/m/Y') . ':';
-        $facture .= "\n\n";
-        $facture .= 'Client : ' . $user->getUsername() . "\n";
-        $facture .= 'Email : ' . $user->getEmail() . "\n";
-        $facture .= "\n";
-        $facture .= 'Détails de la commande :' . "\n";
-        $facture .= 'Produit : ' . $produit->getName() . "\n";
-        $facture .= 'Prix : ' . $produit->getPrice() . ' EUR' . "\n";
-        $facture .= 'Total : ' . $produit->getPrice() . ' EUR' . "\n";
+        // Récupérer les informations du produit associé à l'achat
+        $produit = $achat->getCookingTime();
 
-// Envoyer la facture par e-mail
-        $email = (new \Swift_Message('Facture'))
-            ->setFrom('noreply@example.com')
-            ->setTo($user->getEmail())
-            ->setBody($facture);
-        $this->get('mailer')->send($email);
+        return $this->render('invoice/invoice.html.twig', [
 
-// Rediriger l'utilisateur vers la page d'accueil
-        return $this->redirectToRoute('home');
+            'achat' => $achat,
+            'user' => $user,
+            'produit' => $produit,
+        ]);
     }
+
 }
