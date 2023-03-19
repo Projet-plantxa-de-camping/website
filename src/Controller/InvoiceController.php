440679f5c56077e48cee979a811c26e8bfb6be8d
Antoine <gourgue.antoine@esh64.fr>
2023-03-19 22:54:44 +0100

    update

--- src/Controller/InvoiceController.php
+++ src/Controller/InvoiceController.php
@@ -36,14 +36,16 @@ class InvoiceController extends AbstractController
 
         // Récupérer les informations du produit associé à l'achat
         $produit = $achat->getCookingTime();
-        var_dump($produit);
+
+        // Récupérer l'utilisateur qui a effectué l'achat
+        $user_achat = $achat->getUser();
 
         return $this->render('invoice/invoice.html.twig', [
-
             'achat' => $achat,
-            'user' => $user,
+            'user_achat' => $user_achat,
             'produit' => $produit,
         ]);
+
     }
 
     /**
@@ -82,34 +84,4 @@ class InvoiceController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("admin//invoice/{user_cooking_time_id}", name="_admin_invoice")
-     */
-    public function invoiceAdmin(Request $request, int $user_cooking_time_id): Response
-    {
-        // Récupérer l'utilisateur connecté
-        $user = $this->getUser();
-        $isAdmin = in_array("ROLE_ADMIN", $user->getRoles());
-
-        // Récupérer les informations de l'achat
-        $achat = $this->getDoctrine()->getRepository(UserCookingTime::class)->find($user_cooking_time_id);
-
-        // Vérifier si l'achat appartient à l'utilisateur connecté
-        if (!$isAdmin && (!$achat || $achat->getUser() !== $user)) {
-            throw $this->createNotFoundException('Achat non trouvé');
-        }
-
-        // Récupérer les informations du produit associé à l'achat
-        $produit = $achat->getCookingTime();
-
-        // Récupérer l'utilisateur qui a effectué l'achat
-        $user_achat = $achat->getUser();
-
-        return $this->render('invoice/invoice.html.twig', [
-            'achat' => $achat,
-            'user_achat' => $user_achat,
-            'produit' => $produit,
-        ]);
-
-    }
 }
